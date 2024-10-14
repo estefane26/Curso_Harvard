26d49ecb8732fa4abd959ec549567d52c8ab94f6
Estefane Oliveira <163210937+estefane26@users.noreply.github.com>
2024-10-14 19:02:29 -0300

    Atividade 2 do curso
    Nessa atividade tivemos que aprimorar e acrescentar novas coisas no código enviado anteriormente

---
+++ marioV2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/*A atividade pede que o usuario digite um numero de 1 a 8
+e enquanto o usuario nao digitar o que é solicitado a piramide não é gerada.
+Quando a codição for verdadeira o progama deve imprimir uma piramide de hash's
+alinhada a direita com a altura solicitada ao usuario anteriormente e tambem
+imprimir uma outra piramide logo ao lado alinhado para esquerda*/
+int main(void)
+{
+//Inicializando a Altura
+int h;
+
+//Solicitando ao usuario a altura
+printf("Digite um numero de 1 a 8\n");
+scanf("%i", &h);
+
+//Condicao para verificar se é um numero de 1 a 8
+while(h < 1 || h > 8){
+printf("Digite um numero de 1 a 8\n");
+scanf("%i", &h);
+}
+//A variavel s fará que imprima as hash's na qtde certa
+int s = 0;
+//A variavel p que "empurra" os hash's para que fiquem alinhados a direita
+int p = h;
+//For da linha
+for(int j = 0; j < h; j++){
+
+    s++;
+    p--;
+    //For para imprimir uma qtde de espaço de acordo com a variavel p
+    for(int i= p; i > 0; i--){
+
+        printf(" ");
+    }
+    //For para imprimir a qtde de hash na linha
+    for(int k = 0; k < s; k++){
+       printf("#");
+    }
+    // print para dar os dois espaços entre as piramides
+    printf("  ");
+    //for para imprimir a segunda piramide
+    for(int l = 0; l < s; l++){
+        printf("#");
+    }
+printf("\n");
+}
+
+
+}
+
+
