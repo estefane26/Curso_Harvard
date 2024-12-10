d846d04bf5d05abc3f0b47289fd50ee02b5f4f9f
estefane26 <estefane.sil.oliveira@gmail.com>
2024-12-10 14:57:55 -0300

    Exercício 7
    É um programa que calcula o nível (representado a partir de uma série, como na escola) aproximado necessário para compreender algum texto.

--- legibilidade.c
+++ legibilidade.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <math.h>
+
+
 int QtdeLetras(char texto[]);
 int QtdePalavras(char texto[]);
 int QtdeFrases(char texto[]);
+
 int main(void){
+
     char texto[200];
     printf("Digite um texto: \n");
     fgets(texto, sizeof(texto), stdin);
-    printf("\n letras: %i", QtdeLetras(texto));
-    printf("\n palavras: %i", QtdePalavras(texto));
-    printf("\n frases: %i", QtdeFrases(texto));
+    int p = QtdePalavras(texto);
+    int f = QtdeFrases(texto);
+    int l = QtdeLetras(texto);
+    //Calculo de  Coleman-Liau
+    double L = (double)l / p * 100.0;
+    double S = (double)f / p * 100.0;
+    double result = 0.0588 * L - 0.296 * S - 15.8;
+    /*Verifica se está no intervalo de 1 a 16
+    se estiver um valor menor que 1 é passado como nivel 1
+    se estiver um valor maior que 16 é passado como nivel 16+
+    e arrendonda o numero para o mais proximo */ 
+    if(result < 1){
+        printf("Nivel: 1");
+    }else if (result > 16){
+        printf("Nivel: 16+");
+    }else{
+        result = round(result);
+        printf("Nivel: %i", (int)result);
+    }
 }
-int QtdePalavras(char texto[]){
+// Função para retornar a quantidade de palavras
+/*A logica consiste em pegar o texto e utilizar a condição de verificar o que
+é um caractere de espaço e assim fazer a contagem das palavras, lembrando
+que a quantidade de palavras é a quantidade de espaços mais um*/ 
+    int QtdePalavras(char texto[]){
     int palavras = 0;
     for(int i = 0, n = strlen(texto); i < n; i++){
         if(texto[i] == ' '){
@@ -23,6 +48,9 @@ int QtdePalavras(char texto[]){
     return(palavras);
 }
 
+//Função para retornar a qunatidade de frases
+/*A função verifica quantas pontuções é existente no texto
+e assim calcula a quantidade de frases*/
 int QtdeFrases(char texto[]){
     int frases = 0;
     for(int i = 0, n = strlen(texto); i < n; i++){
@@ -38,7 +66,9 @@ int QtdeFrases(char texto[]){
     }
     return(frases);
 }
-
+//Função para retornar a quantiade de frases
+/*Verifica se o que foi fornecido no texto é uma letra do alfabeto, 
+e faz a contagem de letras*/
 int QtdeLetras(char texto[]){
     int letras = 0; 
     for(int i = 0, n = strlen(texto); i < n; i++){
